6a90527e2adfd2a9132b08caf39f1f5cb43fccb6
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2020-08-27 09:45:05 -0300

    mongo setup

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,11 +1,27 @@
+// Libraries
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express(); 
 
+//Mongo Setup 
+const mongoUri = 'mongodb+srv://admin:7758773S@cluster0.5q8ln.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', ()=>{
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err)=>{
+    console.error('Error connecting to mongo', err)
+});
+
+// Req and Res
 app.get('/', (req, res) => {
     res.send('Hi there')
 }); 
-
+// Port
 app.listen(3000, ()=>{
     console.log('Listening on port 3000');
 });
