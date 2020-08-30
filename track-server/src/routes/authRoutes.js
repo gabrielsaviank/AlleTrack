1b8bc8cb629b040711aa7dc9fdff615374aaf7e0
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2020-08-30 18:43:15 -0300

    jwt implemented

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const User = mongoose.model('User')
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
+
 
 const router = express.Router();
 
@@ -11,7 +13,8 @@ router.post('/signup', async(req, res)=> {
        const user = new User({email, password});
         await user.save();
 
-        res.send('You made a post request');
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
+        res.send({token});
     } catch (err){
         return res.status(422).send(err.message)
     }
