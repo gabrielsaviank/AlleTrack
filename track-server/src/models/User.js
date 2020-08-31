f01a5d4d0dda0b3159ff99bb8dc67622e8022ea3
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2020-08-31 10:00:03 -0300

    adjusts in the salt

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,50 +1,55 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-}); 
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-userSchema.pre('save', function(next){
-    const user = this;
-    if(!user.isModified('password')) {
-        return next();
-    }
-    
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) {
-            return next(err);
-        };
-        bcrypt.hash(user.password, salt, (err, hash) => {
-            if(err){
-                return next(err);
-            }
-            user.password = hash;
-            next();
-        });
-    });
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-userSchema.methods.comparePassword = function(candidatePassword){
-    const user = this;
-    return new Promise((resolve, reject) => {
-        bcryp.compare(candidatePassword, user.password, (err, isMatch) => {
-            if (err){
-                return reject(err);
-            }
-            if (!isMatch) {
-                return reject(false);
-            }
-            resolve(true);
-        })
+userSchema.pre('save', function(next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
     });
-}
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
