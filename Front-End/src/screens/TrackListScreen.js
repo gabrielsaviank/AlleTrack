cc17d389950e0f64b633c9640d1c6266867d338c
Gabriel Savian <gabrielsavian@Gabriels-MacBook-Air.local>
2020-09-01 16:24:35 -0300

    react elements

--- Front-End/src/screens/TrackListScreen.js
+++ Front-End/src/screens/TrackListScreen.js
@@ -4,7 +4,8 @@ import { View, StyleSheet, Text, Button } from 'react-native';
 const TrackListScreen = ({navigation}) => {
   return (
     <View>
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>;
+      <Text style={{fontSize: 48}}>Track List Screen</Text>
+      
       <Button 
         title="Go to track detail"
         onPress={()=>{navigation.navigate('TrackDetail')}}
