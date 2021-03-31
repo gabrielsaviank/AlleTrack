b93b7efb8de09ca9bcd7f8d79e2b400a5f5d39ba
GabrielSavian <gabriel@amplimed.com.br>
2021-03-31 15:12:20 -0300

    wiring auth fields

--- Front-End/src/api/tracker.js
+++ Front-End/src/api/tracker.js
@@ -1,7 +1,7 @@
 // Import Axios to make our life easier with http requests
 import axios from 'axios';
 export default axios.create({
-    baseURL: 'http://ff513bebe496.ngrok.io'
+    baseURL: 'http://167b29d592a6.ngrok.io'
 });
 
 // After 1:30 hours the Ngrok expire and it's necessary to restart the server again
--- Front-End/src/screens/SigninScreen.js
+++ Front-End/src/screens/SigninScreen.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink'
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+  //JSX
+  return(
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+
+      <NavLink 
+        text="Don't have an account? Sign Up Instead" 
+        routeName="Signup"
+
+      />
+    </View>
+  )
 };
 
-const styles = StyleSheet.create({});
+// Deactivate Header
+SigninScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    justifyContent: 'center',
+    marginBottom: 250
+  }
+});
 
 export default SigninScreen;
