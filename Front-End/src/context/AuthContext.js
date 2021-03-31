5b1e42f496f8f8552d82391ad0201ab739402812
GabrielSavian <gabriel@amplimed.com.br>
2021-03-31 15:39:13 -0300

    signin users and changed signup redux

--- Front-End/src/context/AuthContext.js
+++ Front-End/src/context/AuthContext.js
@@ -4,21 +4,23 @@ import trackerApi from '../api/tracker';
 import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
+  // REDUCERS
   switch (action.type) {
     case 'add_error':
       return { ...state, errorMessage: action.payload };
-    case 'signup':
-      return { errorMessage: '', token: action.payload };
+    case 'signin':
+      return {errorMessage: '', token: action.payload}
     default:
       return state;
   }
 };
 
+// Signup
 const signup = dispatch => async ({ email, password }) => {
   try {
     const response = await trackerApi.post('/signup', { email, password });
     await AsyncStorage.setItem('token', response.data.token);
-    dispatch({ type: 'signup', payload: response.data.token });
+    dispatch({ type: 'signin', payload: response.data.token});
 
     navigate('TrackList');
   } catch (err) {
@@ -29,14 +31,24 @@ const signup = dispatch => async ({ email, password }) => {
   }
 };
 
-const signin = dispatch => {
-  return ({ email, password }) => {
-    // Try to signin
+// SignIn
+const signin = dispatch => async({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signin', {email, password});
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({type: 'signin', payload: response.data.token});
+    navigate('TrackList');
+  } catch (error) {
+    dispatch({
+      type: 'add_error',
+      payload: 'Something went wrong with sign in'
+    });
+  }
     // Handle success by updating state
     // Handle failure by showing error message (somehow)
-  };
 };
 
+// Destroy Session
 const signout = dispatch => {
   return () => {
     // somehow sign out!!!
