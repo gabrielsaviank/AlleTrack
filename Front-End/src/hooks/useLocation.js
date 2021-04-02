c0e49f85d4097cde1dbb05ac56d4bf4c749ce7b1
GabrielSavian <gabriel@amplimed.com.br>
2021-04-02 11:04:50 -0300

    hook bug fixed

--- Front-End/src/hooks/useLocation.js
+++ Front-End/src/hooks/useLocation.js
@@ -1,33 +1,35 @@
-import {useState, useEffect} from 'react';
-
+import { useState, useEffect } from 'react';
 import {
-    Accuracy, 
-    requestPermissionAsync,
-    watchPositionAsync
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync,
 } from 'expo-location';
 
 export default (callback) => {
-    const [err, setErr] = useState(null);
+  const [err, setErr] = useState(null);
 
-    //Here we'll start recording user activity
-    const startWatching = async () => {
-        try {
-            await requestPermissionsAsync();
-            await watchPositionAsync(
-                {
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000,
-                    distanceInterval: 10
-                },
-                callback
-            );
-        } catch (e) {
-            setErr(e);
-        };
-    };
-    useEffect(() => {
-        startWatching();
-    }, []);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        callback
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
 
-    return [err];
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  return [err];
 };
